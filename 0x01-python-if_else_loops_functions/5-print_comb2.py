773fac5b2e2602e2e4c235acb2231ade195f0fe4
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-09 14:46:01 -0700

    print combination final

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 for number in range(100):
-    print("{:02d}".format(number), end='')
-    if number != 99:
-        print(", ", end='')
-    else:
-        print()
+    print("{:02d}".format(number), end='' if number == 99 else ", ")
+
+print()
