be6e787d8c35799dbeddd7ebc7c9aa033ad782ee
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-09 14:50:58 -0700

    8 is uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for char in s:
+        if ord(char) >= 97 and ord(char) <= 122:
+            print("{}".format(chr(ord(char) - 32)), end='')
+        else:
+            print("{}".format(char), end='')
+
+    print()
