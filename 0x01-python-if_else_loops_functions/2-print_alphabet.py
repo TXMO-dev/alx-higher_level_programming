6fd2b4d0801f068d3e1a4cb4ea8d7815424afbb5
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-09 14:36:26 -0700

    print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 
-print("".join(chr(c) for c in range(ord('a'), ord('z') + 1)), end="")
-
+print("".join(chr(c) for c in range(ord('a'), ord('z') + 1) if chr(c) not in ['q', 'e']), end="")
