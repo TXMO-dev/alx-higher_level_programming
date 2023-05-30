409c59b2fb4887f33f1d5d730ac3ff0d4b855be3
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-30 16:46:14 -0700

    101 square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,51 +1,135 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
 class Square:
+    """
+    This class represents a square.
+    """
+
     def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+            position (tuple): The position of the square. Default is (0, 0).
+
+        Raises:
+            TypeError: If size is not an integer or position is not a tuple of 2 positive integers.
+            ValueError: If size is less than 0 or position contains non-positive integers.
+
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            int: The size of the square.
+
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
+        """
+        Retrieves the position of the square.
+
+        Returns:
+            tuple: The position of the square.
+
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        """
+        Sets the position of the square.
+
+        Args:
+            value (tuple): The new position of the square.
+
+        Raises:
+            TypeError: If position is not a tuple of 2 positive integers.
+            ValueError: If position contains non-positive integers.
+
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(n, int) for n in value) or \
+                not all(n >= 0 for n in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(num, int) and num >= 0 for num in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Prints the square with the character '#' to stdout.
+
+        If size is 0, it prints an empty line.
+        The square is printed using the position attribute.
+
+        """
         if self.__size == 0:
             print()
-            return
-        for _ in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
 
     def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: The string representation of the square.
+
+        """
+        string = ""
         if self.__size == 0:
-            return ""
-        square_lines = []
-        for _ in range(self.__position[1]):
-            square_lines.append("")
-        for _ in range(self.__size):
-            square_lines.append(" " * self.__position[0] + "#" * self.__size)
-        return "\n".join(square_lines)
+            string += "\n"
+        else:
+            for _ in range(self.__position[1]):
+                string += "\n"
+            for _ in range(self.__size):
+                string += " " * self.__position[0]
+                string += "#" * self.__size
+                string += "\n"
+        return string.strip()
