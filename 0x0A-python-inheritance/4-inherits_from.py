3fc5fb0d6b3222c288c855afb9663cfbc5357072
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-06-12 16:08:31 -0700

    this si the inherit from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this is the inherit from 
+"""
+def inherits_from(obj, a_class):
+    """
+    Check if obj is an instance of a class that inherited from a_class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a class that inherited from a_class,
+        False otherwise.
+    """
+    return isinstance(obj, type) and issubclass(type(obj), a_class) and type(obj) != a_class
