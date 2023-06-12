832e3a8fa0cad790659f1af29b7d83f920197ca6
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-06-12 15:50:06 -0700

    refactored lookup file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+this returns the list of all files.
+"""
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
