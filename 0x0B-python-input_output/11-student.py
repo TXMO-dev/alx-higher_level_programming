1e801b10baeb64371376efe23693c7a2b5935a1b
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-06-13 21:14:09 -0700

    11 students

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this si 11 students
+"""
+class Student:
+    """Class representing a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance with values from json"""
+        for key, value in json.items():
+            setattr(self, key, value)
+
