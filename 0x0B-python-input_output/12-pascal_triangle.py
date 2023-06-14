1fb08bdba1f9de93130fb2e3a33a928948e9afcb
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-06-13 21:15:21 -0700

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this si the pascal triangle
+"""
+def pascal_triangle(n):
+    """Generate Pascal's triangle of n"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
