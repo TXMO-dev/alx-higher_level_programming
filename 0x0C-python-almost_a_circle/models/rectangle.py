88805a8aab014a37b497bac78d0d679548342262
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-06-19 14:58:11 -0700

    added rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+Rectangle Module
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class that inherits from Base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle instance
+        Args:
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+            x (int): x-coordinate of the rectangle's position
+            y (int): y-coordinate of the rectangle's position
+            id (int): Unique id of the rectangle
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        Getter method for width attribute
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for width attribute
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter method for height attribute
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for height attribute
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter method for x attribute
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter method for x attribute
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter method for y attribute
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter method for y attribute
+        """
+        self.__y = value
