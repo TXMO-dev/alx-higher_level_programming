3ea85f90947b274f6b5d060e9eea1adb9560c2c6
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-06-19 15:08:07 -0700

    rectangular

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,9 +107,12 @@ class Rectangle(Base):
     def display(self):
         """
         Prints the Rectangle instance with '#' characters
+        taking into account the x and y coordinates
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
